9007cdf00faafbdc5007bba0400e8d60f6d19327
KunWardhana <kun_wardhana@yahoo.com>
2021-04-30 13:06:39 +0700

    modified:   src/main/resources/application.properties

--- src/main/java/com/example/onlinetutorial/ui/model/response/ClassResponse.java
+++ src/main/java/com/example/onlinetutorial/ui/model/response/ClassResponse.java
@@ -1,11 +1,15 @@
 package com.example.onlinetutorial.ui.model.response;
 
+import com.example.onlinetutorial.shared.dto.UserDTO;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ClassResponse {
     private String classid;
     private String classname;
     private LocalDateTime classDateTime;
+    private UserDTO listUser;
 
     public String getClassid() {
         return classid;
@@ -29,5 +33,13 @@ public class ClassResponse {
 
     public void setClassDateTime(LocalDateTime classDateTime) {
         this.classDateTime = classDateTime;
+    }
+
+    public UserDTO getListUser() {
+        return listUser;
+    }
+
+    public void setListUser(UserDTO listUser) {
+        this.listUser = listUser;
     }
 }
